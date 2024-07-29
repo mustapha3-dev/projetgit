b1b1a40a5f63b16123a5b6b55c278796d2beff2a
Mustapha <jmouston@yahoo.fr>
2024-07-29 12:27:08 +0200

    ajout footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
